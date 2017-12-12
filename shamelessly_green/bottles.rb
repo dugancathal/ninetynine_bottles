4858ac11d675c79281b8650f513fbf9aedbf3f6b
TJ Taylor <dugancathal@gmail.com>
2017-12-12 14:38:59 -0700

    Refactor and collapse 2 and else blocks
    Introduced refactoring style to change code without going red.

--- shamelessly_green/bottles.rb
+++ shamelessly_green/bottles.rb
@@ -16,21 +16,20 @@ class Bottles
         "Go to the store and buy some more, " +
         "99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+        "#{number} #{container(number)} of beer.\n" +
         "Take it down and pass it around, " +
         "no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall."
     else
-      "1 bottle of beer on the wall.\n"
-      "#{number} bottles of beer on the wall, " +
-        "#{number} bottles of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+        "#{number} #{container(number)} of beer.\n" +
         "Take one down and pass it around, " +
-        "#{number-1} bottles of beer on the wall.\n"
+        "#{number-1} #{container(number-1)} of beer on the wall.\n"
     end
+  end
+
+  private
+  def container(size)
+    size != 1 ? "bottles" : "bottle"
   end
 end
