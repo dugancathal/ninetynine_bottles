5685af0646984974f34c013a2537a51b8ad14673
TJ Taylor <dugancathal@gmail.com>
2017-12-13 16:36:38 -0700

    Fix LSP violation for successor to return a BottleNumber

--- shamelessly_green/bottles.rb
+++ shamelessly_green/bottles.rb
@@ -9,26 +9,23 @@ class Bottles
 
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
     "#{bottle_number} of beer on the wall, ".capitalize +
       "#{bottle_number} of beer.\n" +
       bottle_number.obtain_bottles +
-      "#{next_bottle_number} of beer on the wall.\n"
-  end
-
-  private
-
-  def bottle_number_for(number)
-    case number
-      when 0 then BottleNumber0
-      when 1 then BottleNumber1
-      else BottleNumber
-    end.new(number)
+      "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
 class BottleNumber < Struct.new(:number)
+  def self.for(number)
+    case number
+    when 0 then BottleNumber0
+    when 1 then BottleNumber1
+    else BottleNumber
+    end.new(number)
+  end
+
   def container
     "bottles"
   end
@@ -46,7 +43,7 @@ class BottleNumber < Struct.new(:number)
   end
 
   def successor
-    number - 1
+    BottleNumber.for(number - 1)
   end
 
   def to_s
@@ -74,6 +71,6 @@ class BottleNumber0 < BottleNumber
   end
 
   def successor
-    99
+    BottleNumber.for(99)
   end
 end
