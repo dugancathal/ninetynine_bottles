5057518d7ca3d3792d389f97c391bc08c9684c10
TJ Taylor <dugancathal@gmail.com>
2017-12-13 21:09:41 -0700

    Add bottlenumber1

--- shamelessly_green/bottles.rb
+++ shamelessly_green/bottles.rb
@@ -9,37 +9,71 @@ class Bottles
 
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = bottle_number_for(number)
+    next_bottle_number = bottle_number_for(bottle_number.successor)
     "#{bottle_number} of beer on the wall, ".capitalize +
       "#{bottle_number} of beer.\n" +
       bottle_number.obtain_bottles +
       "#{next_bottle_number} of beer on the wall.\n"
   end
+
+  private
+
+  def bottle_number_for(number)
+    case number
+      when 0 then BottleNumber0
+      when 1 then BottleNumber1
+      else BottleNumber
+    end.new(number)
+  end
 end
 
 class BottleNumber < Struct.new(:number)
   def container
-    number == 1 ? "bottle" : "bottles"
+    "bottles"
   end
 
   def pronoun
-    number == 1 ? "it" : "one"
+    "one"
   end
 
   def quantity
-    number == 0 ? "no more" : "#{number}"
+    "#{number}"
   end
 
   def obtain_bottles
-    number == 0 ? "Go to the store and buy some more, " : "Take #{pronoun} down and pass it around, "
+    "Take #{pronoun} down and pass it around, "
   end
 
   def successor
-    number == 0 ? 99 : number - 1
+    number - 1
   end
 
   def to_s
     "#{quantity} #{container}"
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    "no more"
+  end
+
+  def obtain_bottles
+    "Go to the store and buy some more, "
+  end
+
+  def successor
+    99
   end
 end
